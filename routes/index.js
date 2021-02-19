77dc0b691c64ffc5169442464b838e91342c030b
john rokorio <jrokorio52@gmail.com>
2021-02-19 19:37:28 +0300

    form added and landing page modu=ified

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var index=require('../controllers/index');
+var index=require('../controllers/landing');
 
 /* GET home page. */
-router.get('/',index.index);
+router.get('/',landing.get_landing);
 
 module.exports = router;
